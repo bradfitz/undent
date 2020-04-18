cd721dac5ca49957dcbfec1528b0df44c05fd3f9
Matt Singletary <matt.singletary@gmail.com>
2020-04-17 21:25:29 -0400

    add other os clipboards

--- clip_windows.go
+++ clip_windows.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"log"
 	"os/exec"
 )
 
 func getClip() ([]byte, error) {
-	return exec.Command("clip").Output()
+	// this is hacky, but the way simpler than in 
+	// https://github.com/atotto/clipboard/blob/master/clipboard_windows.go
+	return exec.Command("powershell.exe", "Get-ClipBoard").Output()
 }
 
 func pasteClip(out []byte) {
-	// TODO fallback to powershell?
-	panic("unimplemented on windows")
+	cmd := exec.Command("clip")
+	cmd.Stdin = bytes.NewReader(out)
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
