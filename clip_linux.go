276fa28b26a1afc4d9821c268feed3f3729051c1
Matt Singletary <matt.singletary@gmail.com>
2020-04-17 21:25:29 -0400

    add other os clipboards

--- clip_linux.go
+++ clip_linux.go
@@ -8,12 +8,12 @@ import (
 
 func getClip() ([]byte, error) {
 	// TODO what if the linux does not have X (like WSL)
-	return exec.Command("xsel").Output()
+	return exec.Command("xclip", "-selection", "clipboard", "-o").Output()
 }
 
 func pasteClip(out []byte) {
-	// TODO what if the linux does not have X (like WSL)
-	cmd := exec.Command("xsel")
+	// TODO what if the linux does not have X or xclip(like WSL)
+	cmd := exec.Command("xclip", "-selection", "c")
 	cmd.Stdin = bytes.NewReader(out)
 	if err := cmd.Run(); err != nil {
 		log.Fatal(err)
